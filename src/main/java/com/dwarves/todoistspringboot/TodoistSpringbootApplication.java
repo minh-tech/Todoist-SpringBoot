4da9947a9bfcc6bece25630f5074b77d966692e3
Minh Nguyen <minh@example.com>
2020-07-28 11:36:39 -0400

    Spring Boot inittializr

---
+++ src/main/java/com/dwarves/todoistspringboot/TodoistSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.dwarves.todoistspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoistSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoistSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dwarves/todoistspringboot/TodoistSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwarves.todoistspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoistSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
